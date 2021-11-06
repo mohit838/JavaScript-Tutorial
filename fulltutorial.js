3d255bb36793754176f0eabab9ab743c165d9db5
mohit838 <mohitul449@gmail.com>
2021-11-06 20:32:35 +0600

    add more operation

--- fulltutorial.js
+++ fulltutorial.js
@@ -48,3 +48,97 @@ console.log(yourSelf.split('a'));
 
 
 
+//Add, Sub, Mul, Div, Modulo All are the same operation (+, -, *, /, %)
+var numberOne = 10;
+var numberTwo = 20;
+var result = numberOne + numberTwo;
+console.log(result); //Output: 30
+
+
+//**Special Case
+var numOne = 'abc';
+var numTwo = 20;
+var result = numOne + numTwo;
+console.log(result); //Output: abc20
+
+
+var numOne = 'abc';
+var numTwo = 'xyz';
+var result = numOne + numTwo;
+console.log(result); //Output: abcxyz
+
+
+var numOne = 'abc';
+var numTwo = 'xyz';
+console.log(numOne+" "+numTwo); //Output: abc xyz
+
+
+//String to number conversion [It's maybe Float or Int]
+var numOne = 10;
+var numTwo = '20.5';
+numTwo = parseFloat(numTwo);
+numThree = parseInt(numTwo);
+var result = numOne + numTwo;
+var result2 = numOne + numThree;
+console.log(result); //Output: 30.5
+console.log(result2); //Output: 30
+
+
+//Increment Number
+var numOne = 10;
+var numTwo = 20.5;
+var numThree = 21.5;
+var numFour = 22.5
+
+var result1 = numTwo + 1; //Increment First
+var result2 = ++numThree; //Increment First
+var result3 = numFour++; //Using in For/While Loop; Increment Later
+
+console.log(result1); //Output: 21.5
+console.log(result2); //Output: 22.5
+console.log(result3); //Output: 22.5
+
+
+
+//**Special Case
+var numOne = 10;
+
+var result1 = '' + numOne; //Empty string
+var result2 = ' ' + numOne; //Space here
+console.log(result1); //Output: 10
+console.log(result2); //Output: 10
+
+//Look Carefully
+console.log(typeof result1); //Output: string
+console.log(typeof result2); //Output: string
+
+
+//Floating Addition
+var numOne = 10.02;
+var numTwo = 15.20;
+
+var result = numOne + numTwo;
+console.log(result); //Output: 25.22
+
+//**Special Case for Floating
+var numOne = 0.1;
+var numTwo = 0.2;
+
+console.log(numOne + numTwo); //Output: 0.30000000000000004
+
+
+var numOne = 0.1;
+var numTwo = 0.2;
+
+var sum = numOne + numTwo;
+sum = sum.toFixed(2);
+console.log(sum); //Output: 0.30
+
+
+
+
+
+
+
+
+
