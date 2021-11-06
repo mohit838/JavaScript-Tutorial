09fc454a2c83b323f9a9c32f99016db363e76453
mohit838 <mohitul449@gmail.com>
2021-11-06 14:37:08 +0600

    variable in js

--- variable.js
+++ variable.js
@@ -1,1 +1,29 @@
-//variable in Js
+//Hello in Js
+
+console.log(25);
+console.log("Hello World!");
+console.log('Second Hello World!');
+
+//Variable
+//Note 1: Variable must be "camelCase".
+//Note 2: "Semicolon" after every line.
+//Note 3: "caseSensitive".
+
+
+//Number
+var priceOfPen = 55;
+console.log(priceOfPen); //Output: 55
+console.log(typeof priceOfPen); //Output: Number
+
+
+//String
+var yourName = 'XyZ';
+console.log(yourName); //Output: XyZ
+console.log(typeof yourName); //Output: String
+
+
+//Boolean
+var isHot = true;
+console.log(isHot); //Output: String
+console.log(typeof isHot); //Output: Boolean
+
