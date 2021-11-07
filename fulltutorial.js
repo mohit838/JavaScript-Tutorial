56cd9739a8d918231d35d0297d0a07071716deb0
mohit838 <mohitul449@gmail.com>
2021-11-07 13:37:25 +0600

    loop

--- fulltutorial.js
+++ fulltutorial.js
@@ -278,5 +278,15 @@ array.shift(); //Remove Element From Beginning
 console.log(array); //Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
+//Simple While Loop
+
+var num = 0;
+
+while (num < 5) {
+    console.log(num); //Output: 0 1 2 3 4
+    num++;
+}
+
+
 
 
--- test.js
+++ test.js
@@ -1,3 +1,11 @@
+//Simple While Loop
+
+var num = 0;
+
+while (num < 5) {
+    console.log(num); //Output: 0 1 2 3 4
+    num++;
+}
 
 
 
