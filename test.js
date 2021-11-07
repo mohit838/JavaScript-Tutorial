561df7cecbeb874dddbb1540e070f4a03a6c33d5
mohit838 <mohitul449@gmail.com>
2021-11-07 11:57:37 +0600

    add abs

--- test.js
+++ test.js
@@ -1,4 +1,20 @@
+//Absolute Number
 
+var numOne = -10;
+var absoluteNum = Math.abs(numOne);
+
+console.log(absoluteNum); //Output 10
+
+
+//Round and Ceiling any fraction
+var numOne = -10.20;
+var absoluteNum = Math.abs(numOne);
+var roundNum = Math.round(absoluteNum);
+var ceilingNum = Math.ceil(absoluteNum);
+
+console.log(absoluteNum); //Output: 10.2
+console.log(roundNum); //Output: 10
+console.log(ceilingNum); //Output: 11
 
 
 
