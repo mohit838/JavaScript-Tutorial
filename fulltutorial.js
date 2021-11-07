7f81575ef483511023dcdb5226a8f761d69701b1
mohit838 <mohitul449@gmail.com>
2021-11-07 21:10:48 +0600

    add more code

--- fulltutorial.js
+++ fulltutorial.js
@@ -561,10 +561,69 @@ console.log(unique); //Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 
+//Count White Spaces
+
+var text = 'I  am  a  JavaScript';
+var count = 0;
+
+for (var i = 0; i <text.length; i++) {
+    var char = text[i];
+    if (char == ' ' && text[i-1] == ' ') {
+        count++;
+    }
+
+}
+
+console.log(count); //Output: 3
 
 
 
 
+//Reverse  String
+
+function reverseString(str) {
+    var reverse = "";
+
+    for (var i = 0; i < str.length; i++){
+        var char = str[i];
+        reverse = char + reverse;
+    }
+    return reverse;
+}
+
+console.log(reverseString("I am Js"));
+
+
+
+//Pallindrome Number or String
+
+function reverseString(str) {
+    var reverse = "";
+
+    for (var i = 0; i < str.length; i++){
+        var char = str[i];
+        reverse = char + reverse;
+    }
+    return reverse;
+}
+
+var str ='DaD';
+var str2 ='Dude';
+
+if(str == reverseString(str)){
+    console.log("This is Pallindrome"); //Output: This is Pallindrome 
+}
+else{
+    console.log("This is not a Pallindrome");
+}
+
+
+if(str2 == reverseString(str2)){
+    console.log("This is Pallindrome"); 
+}
+else{
+    console.log("This is not a Pallindrome"); //Output: This is Not a Pallindrome 
+}
 
 
 
--- test.js
+++ test.js
@@ -1,3 +1,53 @@
+//Reverse  String
+
+function reverseString(str) {
+    var reverse = "";
+
+    for (var i = 0; i < str.length; i++){
+        var char = str[i];
+        reverse = char + reverse;
+    }
+    return reverse;
+}
+
+console.log(reverseString("I am Js"));
+
+
+
+//Pallindrome Number or String
+
+function reverseString(str) {
+    var reverse = "";
+
+    for (var i = 0; i < str.length; i++){
+        var char = str[i];
+        reverse = char + reverse;
+    }
+    return reverse;
+}
+
+var str ='DaD';
+var str2 ='Dude';
+
+if(str == reverseString(str)){
+    console.log("This is Pallindrome"); //Output: This is Pallindrome 
+}
+else{
+    console.log("This is not a Pallindrome");
+}
+
+
+if(str2 == reverseString(str2)){
+    console.log("This is Pallindrome"); 
+}
+else{
+    console.log("This is not a Pallindrome"); //Output: This is Not a Pallindrome 
+}
+
+
+
+
+
 
 
 
