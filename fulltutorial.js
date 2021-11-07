c45f0b9a6eaa13dfbb10933c0c3950f29f2457a0
mohit838 <mohitul449@gmail.com>
2021-11-07 14:12:37 +0600

    function

--- fulltutorial.js
+++ fulltutorial.js
@@ -326,3 +326,34 @@ switch (num){
 }
 
 
+//Function in Js
+
+function addNum(a, b){
+    var a; var b;
+    var result = a + b;
+    return result;
+}
+
+var total = addNum(5, 10);
+
+console.log(total); //Output: 15
+
+
+
+//**Look Carefully
+function addNum(a, b){
+    var result = a + b;
+    return result;
+}
+
+var total1 = addNum(15, 10);
+var total2 = addNum(50, 100);
+
+console.log(total1); //Output: 25
+console.log(total2); //Output: 150
+
+//Another Method of Showing Result
+console.log(total1, total2); //Output: 25 150
+
+
+
--- test.js
+++ test.js
@@ -1,16 +1,34 @@
-//Switch Case in Js
+//Function in Js
 
-var num = 1;
-
-switch (num){
-    case 1:
-        console.log(num);
-        break;
-    
-    default:
-        console.log("Error Entry!!!");
-        break;
+function addNum(a, b){
+    var a; var b;
+    var result = a + b;
+    return result;
 }
+
+var total = addNum(5, 10);
+
+console.log(total); //Output: 15
+
+
+
+//**Look Carefully
+function addNum(a, b){
+    var result = a + b;
+    return result;
+}
+
+var total1 = addNum(15, 10);
+var total2 = addNum(50, 100);
+
+console.log(total1); //Output: 25
+console.log(total2); //Output: 150
+
+//Another Method of Showing Result
+console.log(total1, total2); //Output: 25 150
+
+
+
 
 
 
